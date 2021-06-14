67c4893bb79b0089fc4eb510d7f54bf52d333975
Vilay Bende <71513950+vilay1702@users.noreply.github.com>
2021-06-14 21:11:49 +0530

    added themes

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ AOS.init({
   duration: 500,
 });
 
+// Light And Dark Theme
+let body = document.getElementById("mainBody");
+let logo = document.getElementById("logo");
+logo.addEventListener("click", () => {
+  if (body.classList == "lightTheme") {
+    body.classList.add("darkTheme");
+    body.classList.remove("lightTheme");
+  } else {
+    body.classList.remove("darkTheme");
+    body.classList.add("lightTheme");
+  }
+});
+
 // Read More Button
 function readMore(e) {
   if (e.innerHTML == "Read More") {
@@ -14,4 +27,15 @@ function readMore(e) {
   e.parentElement.classList.toggle("readMore");
 }
 
-let x = document.getElementById("searchBar");
+function searchBlog(e) {
+  Array.from(items).forEach((item) => {
+    if (
+      item.textContent.toLowerCase().indexOf(searchBar.value.toLowerCase()) !=
+      -1
+    ) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
